175fce39a945b8fb13e709dd7aed506d09a77d2b
wluu38 <wluu@worcester.edu>
2024-11-06 13:15:43 -0500

    Updated image upload feature to add images dynamically

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 let uploadButton = document.getElementById("file-upload");
-let chosenImage = document.getElementById("chosenImage");
+let gallery = document.getElementById("gallery");
 
 uploadButton.onchange = () => {
-    let reader = new FileReader();
-    reader.readAsDataURL(uploadButton.files[0]);
-    console.log(uploadButton.files[0]);
-    reader.onload = () => {
-        chosenImage.setAttribute("src", reader.result);
+    let file = uploadButton.files[0];
+    if(file) {
+        let fileExtension = file.name.split('.').pop().toLowerCase();
+        if(fileExtension === "jpg" || fileExtension === "png" || fileExtension === "jpeg") {
+            let reader = new FileReader();
+            reader.readAsDataURL(file);
+            console.log(uploadButton.files[0]);
+    
+            reader.onload = () => {
+                let imgLink = document.createElement("a");
+                imgLink.href = reader.result;
+                imgLink.setAttribute("data-lightbox", "models");
+                imgLink.setAttribute("data-title", "New Image");
+    
+                let img = document.createElement("img");
+                img.src = reader.result;
+    
+                imgLink.appendChild(img);
+                gallery.appendChild(imgLink);
+            }
+        }
+        else {
+            alert("Please select a valid image file (.jpg or .png)")
+        }
     }
-}
+};
